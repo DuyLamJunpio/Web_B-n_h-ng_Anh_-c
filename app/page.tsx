f490b1f2c70ec0b473e141c2fae37a7dcbed0931
Duy Lam <duylamjunpio@gmail.com>
2026-09-23 12:57:40 +0700

    Remove Collections section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/Header";
 import HeroVideo from "@/components/HeroVideo";
 import CategoryBand from "@/components/CategoryBand";
-import Collections from "@/components/Collections";
 import FeaturedStoryVideo from "@/components/FeaturedStoryVideo";
 import About from "@/components/About";
 import Footer from "@/components/Footer";
@@ -13,7 +12,6 @@ export default function Home() {
       <main className="relative z-10">
         <HeroVideo />
         <CategoryBand />
-        <Collections />
         <FeaturedStoryVideo />
         <About />
       </main>
